84f914186e4d5717818a5ce458d48df38766b747
Alex Pogran <pograncheg@gmail.com>
2024-07-08 00:42:14 +0300

    Исправления в задача 3 по двумерным массивам.

--- twoDimArray/task3/index.php
+++ twoDimArray/task3/index.php
@@ -1,8 +1,8 @@
 <?php
 $n = mt_rand(3,5);
 $m = mt_rand(3,5);
-echo $n . ' x ' . $m . '<br>';
-function randomArray($n, $m) {
+// echo $n . ' x ' . $m . '<br>';
+function randomArray(int $n, int $m) {
     $arr = [];
     for ($i = 0; $i < $n; $i++) {
         $arr[$i] = range(1, 1 + $m - 1);
@@ -11,7 +11,6 @@ function randomArray($n, $m) {
             $arr[$i][$j] = $arr[$i][$j] * mt_rand(0, 13);
         }
         $sort = mt_rand(-1, 1);
-        echo $sort . '<br>';
         switch ($sort) {
             case -1:
                 rsort($arr[$i]);
@@ -27,7 +26,7 @@ function randomArray($n, $m) {
     return $arr;
 }
 
-function isSort($arr) {
+function isSort(array $arr) {
     $asc = true;
     $desc = true;
     for ($i = 0; $i < count($arr)-1; $i++) {
@@ -36,16 +35,16 @@ function isSort($arr) {
             break;
         }
     }
-    if ($desc = true) {
+    if ($desc == true) {
         return true;
     }
     for ($i = 0; $i < count($arr)-1; $i++) {
         if($arr[$i + 1] < $arr[$i]) {
-            $asc= false;
+            $asc = false;
             break;
         }
     }
-    if ($asc = true) {
+    if ($asc == true) {
         return true;
     }
 }
@@ -55,6 +54,51 @@ echo 'Исходный массив: <br>' . '<pre>';
 print_r($arr);
 echo '</pre>';
 
-for ($i = 0; $i < $n; $i++) {
-    
+function minElInArr(array $arr) {
+    $min = $arr[0];
+    for ($i = 1; $i < count($arr); $i++) {
+        if ($arr[$i] < $min) {
+            $min = $arr[$i]; 
+        }
+    }
+    return $min;
+}
+
+function maxElInArr(array $arr) {
+    $max = $arr[0];
+    for ($i = 1; $i < count($arr); $i++) {
+        if ($arr[$i] > $max) {
+            $max = $arr[$i]; 
+        }
+    }
+    return $max;
+}
+
+function maxMinInOrderedLine(array $arr) {
+    for ($i = 0; $i < count($arr); $i++) {
+        if (isSort($arr[$i])) {
+            $max = maxElInArr($arr[$i]);
+            $min = minElInArr($arr[$i]);
+            $index = $i;
+            break;
+        }
+    }
+    if (isset($index) && isset($max) && isset($min)) {
+        for ($i = $index + 1; $i < count($arr); $i++) {
+            if (isSort($arr[$i])) {
+                $max = maxElInArr($arr[$i]) > $max ? maxElInArr($arr[$i]) : $max;
+                $min = minElInArr($arr[$i]) < $min ? minElInArr($arr[$i]) : $min;
+            }
+        }
+        return [$min, $max];
+    }
+    return ['Нет упорядоченных строк'];
+}
+
+$result = maxMinInOrderedLine($arr);
+if(count($result) == 1) {
+    echo $result[0];
+} else {
+    echo 'Минимальный элемент в упорядоченных строках: ' . $result[0] . '<br>';
+    echo 'Максимальный элемент в упорядоченных строках: ' . $result[1]. '<br>';
 }
