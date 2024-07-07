c6b1f0d4ca8e3b75d3ba4e6e6e34050b962858f9
Alex Pogran <pograncheg@gmail.com>
2024-07-07 16:39:08 +0300

    Исправление задачи 4 на одномерные массивы.

--- array/task4/index.php
+++ array/task4/index.php
@@ -1,22 +1,19 @@
 <?php
 
-$arr = [];
-$n = mt_rand(5,15);
-for ($i = 0; $i <=$n; $i++) {
-    $arr[$i] = mt_rand(0, 12);
-}
+$arr = range(0, 25, 3);
 sort($arr);
 echo 'Исходный массив:';
 echo '<pre>';
 print_r($arr);
 echo '</pre>';
+
 $k = mt_rand(1,3);
-// echo 'Вставим в массив' . $k . ' элементов.<br>';
-$a = [0, 2, 3, 5, 6, 8, 9];
+echo 'Количество вставляемых элементов: ' . $k . '<br>';
+
 function insertEl(int $k, array $arr) {
-    for ($j = 1; $j <= $k; $k++) {
-        $el = mt_rand(1, 10);
-        // echo $el . '<br>';
+    for ($q = 1; $q <= $k; $q++) {
+        $el = mt_rand(1, 13);
+        echo 'Вставляем значение: ' . $el . '<br>';
         for ($i = 0; $i < count($arr); $i++) {
             if ($arr[$i] > $el) {
                 for ($j = count($arr) - 1; $j >= $i; $j--) {
@@ -26,14 +23,11 @@ function insertEl(int $k, array $arr) {
                 break;
             }
         }
-        // echo '<pre>';
-        // print_r($arr);
-        // echo '</pre>';
     }
     return $arr;
 }
 
-
+echo 'Результат:';
 echo '<pre>';
-print_r(insertEl($k, $a));
+print_r(insertEl($k, $arr));
 echo '</pre>';
