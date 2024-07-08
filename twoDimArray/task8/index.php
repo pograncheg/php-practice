a26d0f4880bbea5985cb1a776b798adddc08be8f
Alex Pogran <pograncheg@gmail.com>
2024-07-08 23:56:05 +0300

    Решение задачи №8 на двумерные массивы.

--- twoDimArray/task8/index.php
+++ twoDimArray/task8/index.php
@@ -1,12 +1,12 @@
 <?php
 $n = mt_rand(3,5);
-$m = mt_rand(3,5);
+$m = mt_rand(5,10);
 function randomArray(int $n, int $m) 
 {
     $arr = [];
     for ($i = 0; $i < $n; $i++) {
         for ($j = 0; $j < $m; $j++) {
-            $arr[$i][$j] = mt_rand(-5, 50);
+            $arr[$i][$j] = mt_rand(-3, 7);
         }
     }
     return $arr;
@@ -17,6 +17,35 @@ echo 'Исходный массив: <br>' . '<pre>';
 print_r($arr);
 echo '</pre>';
 
-$countRow = count($arr);
-$countCol = count($arr[0]);
+function deleteBetweenMax(array $arr) {
+    $countRow = count($arr);
+    $countCol = count($arr[0]);
+    for ($i = 0; $i < $countRow; $i++) {
+        $max = $arr[$i][0];
+        $firstMaxIndex = 0;
+        $lastMaxIndex = 0;
+        for ($j = 1; $j < $countCol; $j++){
+            if ($arr[$i][$j] >= $max) {
+                $max = $arr[$i][$j];
+                $lastMaxIndex = $j;
+            }
+        }
+        for ($j = 0; $j <= $lastMaxIndex; $j++) {
+            if ($arr[$i][$j] == $max) {
+                $firstMaxIndex = $j;
+                break;
+            } 
+        }
+        // echo 'first ' . $firstMaxIndex . ' and last ' . $lastMaxIndex . '<br>';
+        for ($j = $firstMaxIndex +1; $j < $lastMaxIndex; $j++) {
+            unset($arr[$i][$j]);
+        }
+    }
+    return $arr;
+}
+
+$result = deleteBetweenMax($arr);
+echo 'Итоговый массив: <br>' . '<pre>';
+print_r($result);
+echo '</pre>';
 
