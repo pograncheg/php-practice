f013ac5beefc9b19d33cdf81c8e098558ff8f828
Alex Pogran <pograncheg@gmail.com>
2024-07-08 12:23:33 +0300

    Решение задачи 5 на двумерные массивы.

--- twoDimArray/task5/index.php
+++ twoDimArray/task5/index.php
@@ -1,14 +1,16 @@
 <?php
 $n = mt_rand(3,5);
 $m = mt_rand(3,5);
-
-function randomArray(int $n, int $m) {
+// $p = mt_rand(2,9);
+// echo 'Проверяем кратность: ' . $p . '<br>';
+function randomArray(int $n, int $m) 
+{
     $arr = [];
     for ($i = 0; $i < $n; $i++) {
         $arr[$i] = range(1, 1 + $m - 1);
         shuffle($arr[$i]);
         for ($j = 0; $j < $m; $j++) {
-            $arr[$i][$j] = $arr[$i][$j] * mt_rand(0, 53);
+            $arr[$i][$j] = $arr[$i][$j] * mt_rand(0, 12);
         }
     }
     return $arr;
@@ -18,3 +20,38 @@ $arr = randomArray($n, $m);
 echo 'Исходный массив: <br>' . '<pre>';
 print_r($arr);
 echo '</pre>';
+
+function sumColEl(array $arr, int $col) 
+{
+    $sum = 0;
+    for ($i = 0, $countRow = count($arr); $i < $countRow; $i++) {
+        $sum += $arr[$i][$col];
+    }
+    return $sum;
+}
+
+function sumElMultipleOf(array $arr, int $p) 
+{
+    $sum = null;
+    for ($j = 0, $countCol = count($arr[0]); $j < $countCol; $j++) {
+        for ($i = 0, $countRow = count($arr); $i < $countRow; $i++) {
+            $multiple = true;
+            if ($arr[$i][$j] % $p != 0) {
+                $multiple = false;
+                break;
+            }
+        }
+        if ($multiple) {
+            $sum += sumColEl($arr, $j);
+        }
+    }
+    return $sum;
+}
+
+$sum = sumElMultipleOf($arr, 3);
+
+if ($sum === null) {
+    echo 'Нет подходящих столбцов!';
+} else {
+    echo 'Сумма = ' . $sum . '<br>';
+}
