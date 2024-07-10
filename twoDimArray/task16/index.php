fc319821860aeff8388398f58749282777437421
Alex Pogran <pograncheg@gmail.com>
2024-07-10 23:01:42 +0300

    Двумерные массивы. Решение задачи №16

--- twoDimArray/task16/index.php
+++ twoDimArray/task16/index.php
@@ -19,30 +19,55 @@ echo 'Исходный массив: <br>' . '<pre>';
 print_r($arr);
 echo '</pre>';
 
+function mySort(array $arr, array $indexMaxEl) 
+{
+    $count = count($arr);
+    for ($j = 0; $j < $count; $j++) {
+        $f = false;
+        for ($q = 0; $q < $count - 1 - $j; $q++) {
+            if ($arr[$q][$indexMaxEl[$q]] < $arr[$q+1][$indexMaxEl[$q+1]]) {
+                $buff = $arr[$q + 1];
+                $arr[$q + 1] = $arr[$q];
+                $arr[$q] = $buff;
+                $buffIndex = $indexMaxEl[$q+1];
+                $indexMaxEl[$q+1] = $indexMaxEl[$q];
+                $indexMaxEl[$q] = $buffIndex;
+                $f = true;
+            }
+        }
+        if (!$f) {
+            break;
+        }
+    } 
+    return $arr;
+}
+
 function main(array $arr)
 {
     $countCol = count($arr[0]);
     $countRow = count($arr);
     $maxElInRow = [];
+    $indexMaxEls = [];
     $rowOrder = [];
     $rowForInsert = 0;
     for ($i = 0; $i < $countRow; $i++) {
         $maxEl = $arr[$i][0];
+        $indexMaxEl = 0;
         for ($j = 0; $j < $countCol; $j++) {
             if ($arr[$i][$j] > $maxEl) {
+                $maxEl = $arr[$i][$j];
                 $indexMaxEl = $j;
             }
         }
-        $indexMaxElInRow[$i] = $IndexMaxEl;
+        $maxElInRow[$i] = $maxEl;
+        $indexMaxEls[$i] = $indexMaxEl;
     }
-    echo 'Массив максимальных элементов: <br>' . '<pre>';
-    print_r($maxElInRow);
-    echo '</pre>';
-    // for ($i = 0; $i < $countRow - 1; $i++) {
-    //     for ($j = 0; $j < $countRow - 1 - i; $j++){
-    //         if ($arr[$j][$indexMaxElInRow[$i]] > $arr[$j])
-    //     }
-    // }
+
+    return mySort($arr, $indexMaxEls);
+
 }
 
-main($arr);
+$result = main($arr);
+echo 'Итоговый массив: <br>' . '<pre>';
+print_r($result);
+echo '</pre>';
