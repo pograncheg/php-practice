9791636408facc1342cbb6beae7060dfeef93143
Alex Pogran <pograncheg@gmail.com>
2024-07-15 14:15:23 +0300

    Целочисленная арифметика. Исправление задачи №3

--- integer/task3/index.php
+++ integer/task3/index.php
@@ -1,30 +1,27 @@
 <?php
 //Задача 3
 for ($k = 1000; $k < 10000; $k++) {
+    if ($k % 2 <> 0) {
+        continue;
+    } 
     $b = true;
     $num = $k % 10;
     $n = intdiv($k, 10);
-    if ($n % 10 == $num) {
+    $sign = $num - ($n % 10);
+    if ($sign == 0) {
         $b = false;
-    } elseif ($n % 10 < $num) {
-        while ($n >= 10) {
-            $num = $n % 10;
-            $n = intdiv($n, 10);
-            if ($n % 10 >= $num) {
-                $b = false;
-                break;
-            }
-        }
     } else {
         while ($n >= 10) {
             $num = $n % 10;
             $n = intdiv($n, 10);
-            if ($n % 10 <= $num) {
+            if ($sign * ($num - ($n % 10)) <= 0) {
                 $b = false;
                 break;
             }
         }
     }
+
+
     if ($b) {
         echo $k . '<br>';
     }
